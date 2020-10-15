8ca3936271fb31a3fcede1a8cba2e927582d425d
dwiky dias <diaslaksmana@gmail.com>
2020-10-15 07:51:01 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,4 +33,22 @@
             app.onResize();
         });
     });
+
+    $(function() {
+        var header = $(".header");
+         
+        $(window).scroll(function() {    
+          var scroll = $(window).scrollTop();
+          if (scroll >= 50) {
+            header.addClass("scrolled");
+          } 
+          else {
+            header.removeClass("scrolled");
+          }
+        });
+        
+      });
+
+    
+
 });
